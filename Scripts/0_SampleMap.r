9abf67669580d87db40895fddd101b92b8e90cad
Alexandra J. van Rensburg <alexjvr1@users.noreply.github.com>
2022-09-15 21:41:51 +0100

    Create 0_SampleMap.r

---
+++ Scripts/0_SampleMap.r
@@ -0,0 +1,42 @@
+##Draw sample map and write to pdf
+## Input file "BA_SiteInfo"
+
+library(sp)  # classes for spatial data
+library(raster)  # grids, rasters
+library(rasterVis)  # raster visualisation
+library(maptools)
+library(rgeos)
+
+
+##get UK map
+
+require(spatial.tools)
+elevation<-getData("alt", country = "GB")
+x <- terrain(elevation, opt = c("slope", "aspect"), unit = "degrees")
+plot(x)
+slope <- terrain(elevation, opt = "slope")
+aspect <- terrain(elevation, opt = "aspect")
+hill <- hillShade(slope, aspect, 40, 270)
+plot(hill, col = grey(0:100/100), legend = FALSE, main = "UK")
+#plot(elevation, col = rainbow(25, alpha = 0.35), add = TRUE)
+
+##BA data points
+BA.SiteInfo <- read.table("BA_SiteInfo", header=T)  ##read table of info into R
+UK.coords <- BA.SiteInfo ##change name of file so that it can be manipulated
+coordinates(UK.coords) <- c("Long", "Lat")  # set spatial coordinates. Remember to check that these make sense
+#plot(CH_coords)
+#plot(CH_coords, pch = 20, cex = 1, col = "black")
+crs.geo <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
+proj4string(UK.coords) <- crs.geo  # define projection system of our data
+summary(UK.coords)
+
+##add columns for colour (host plant) and shape (new vs old sites). 
+
+UK.coords$pch <- c(17, 15, 17,15, 15, 15, 17, 15, 17)
+UK.coords$Colours <- c("gold1", "gold1", "darkorchid3", "gold1", "darkorchid3", "gold1", "darkorchid3", "gold1", "darkorchid3")
+
+##plot map
+pdf("BA.sampleMap_20180619.pdf")
+plot(hill, col = grey(0:100/100), legend = FALSE, main = "UK")
+plot(UK.coords, pch=UK.coords$pch, cex=2, col=UK.coords$Colours, add=T)
+dev.off()
